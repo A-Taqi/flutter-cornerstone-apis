ed366a3187f03f2b061d359e82fd6486ef4d0862
taqi <a2taqi@gmail.com>
2024-10-30 09:37:44 +0300

    tasky - adds more leave types

--- app/Http/Controllers/Project2/LeaveRequestsController.php
+++ app/Http/Controllers/Project2/LeaveRequestsController.php
@@ -23,7 +23,7 @@ class LeaveRequestsController extends Controller
         $request->validate([
             'start_date' => 'required|date',
             'end_date' => 'required|date',
-            'type' => 'required|in:annual,sick',
+            'type' => 'required|in:annual,sick,casual,maternity,paternity,unpaid',
         ]);
 
         $leaveRequest = new LeaveRequest();
